9f43a678b7bcdc1a70bdf22bacaea57162168d88
Judgement9882 <kjy6135@inu.ac.kr>
2022-04-01 10:10:24 +0900

    Create buttons.py

---
+++ 5_PiFaceCAD/buttons.py
@@ -0,0 +1,14 @@
+# test button
+
+import pifacecad
+def update_pin_text(event):
+    event.chip.lcd.set_cursor(13, 0)
+    event.chip.lcd.write(str(event.pin_num))
+
+cad = pifacecad.PiFaceCAD()
+cad.lcd.write("You Pressed: ")
+listener = pifacecad.SwitchEventListener(chip=cad)
+for i in range(8):
+    listener.register(i, pifacecad.IODIR_FALLING_EDGE, update_pin_text)
+
+listener.activate()
